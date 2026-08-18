214d69162124091661c5a4811664acd343be77fd
satalight <114524244+satasatalight@users.noreply.github.com>
2026-08-18 18:55:41 -0400

    Add: two-integer-sum-ii - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-9.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        HashSet<Integer> searched = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int search = target - numbers[i];
+
+            if (searched.contains(search))
+                continue;
+
+            int found;
+
+            // edge case, searched value is equal to self
+            if (search == numbers[i]) {
+                if (numbers[i + 1] == search)
+                    return new int[] {i + 1, i + 2};
+                else
+                    continue;
+            }
+
+            else if (search > numbers[i])
+                found = Arrays.binarySearch(numbers, i + 1, numbers.length, search);
+
+            else 
+                found = Arrays.binarySearch(numbers, 0, i, search);
+
+            if (found >= 0)
+                return new int[] {i + 1, found + 1};
+
+            searched.add(search);
+        }
+
+        return null;
+    }
+}
